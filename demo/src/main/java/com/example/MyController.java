cefb6717864212b11472cbdea172c36c31591c78
ROHIT <rohit.agarwal@accionlabs.com>
2022-01-28 17:42:43 +0530

    service discovery identify changes

--- demo/src/main/java/com/example/MyController.java
+++ demo/src/main/java/com/example/MyController.java
@@ -3,16 +3,25 @@ package com.example;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.runtime.server.EmbeddedServer;
 
 import java.util.UUID;
 
 @Controller("/appinfo")
 public class MyController {
+    protected final String host;
+
+    protected final int port;
+
+    public MyController(EmbeddedServer embeddedServer) {
+        host = embeddedServer.getHost();
+        port = embeddedServer.getPort();
+    }
 
     private String uuid = UUID.randomUUID().toString();
 
     @Get(produces = MediaType.APPLICATION_JSON, value = "uuid")
     public String getUUID() {
-        return "{ uuid: '" + uuid + "'}";
+        return "demo--"+ host + ":" + port +" { uuid: '" + uuid + "'}";
     }
 }
--- ribbonTest/src/main/java/com/netflix/RemoteService.java
+++ ribbonTest/src/main/java/com/netflix/RemoteService.java
@@ -3,7 +3,7 @@ package com.netflix;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.client.annotation.Client;
 
-@Client("demo")
+@Client(id="demo")
 public interface RemoteService {
 
     @Get("/appinfo/uuid")
